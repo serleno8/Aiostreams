d9c5e228aadeb9db807b3dcd833bd58a6a34fcef
sertoo123 <toon.serraes@live.be>
2025-01-29 17:01:34 +0100

    Update base.ts

--- packages/wrappers/src/base.ts
+++ packages/wrappers/src/base.ts
@@ -13,7 +13,6 @@ import {
   serviceDetails,
   Settings,
 } from '@aiostreams/utils';
-import { fetch as uFetch, ProxyAgent } from 'undici';
 import { emojiToLanguage, codeToLanguage } from '@aiostreams/formatters';
 
 export class BaseWrapper {
@@ -112,19 +111,7 @@ export class BaseWrapper {
           );
         }
       } catch (error: any) {
-        if (!Settings.ADDON_PROXY) {
-          throw error;
-        }
-        const dispatcher = new ProxyAgent(Settings.ADDON_PROXY);
-        console.error(
-          `|ERR| wrappers > base > ${this.addonName}: Got error: ${error.message} when fetching from ${sanitisedUrl}, trying with proxy instead`
-        );
-        response = await uFetch(url, {
-          dispatcher,
-          method: 'GET',
-          headers: headers,
-          signal: controller.signal,
-        });
+throw error;
       }
 
       clearTimeout(timeout);
